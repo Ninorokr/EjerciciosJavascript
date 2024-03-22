0bda5814d1dadb26ac5df733997d29fcd41501d1
AlvaroG <ninorokr2@gmail.com>
2024-03-21 20:36:16 -0500

    6 de 10 ejercicios, clase 1. Pendiente clase 2

---
+++ 1-variables/scripts.js
@@ -0,0 +1,56 @@
+//EJERCICIO 1
+//Escribir un programa que muestre por pantalla la cadena ¡Hola Mundo!.
+
+alert("¡Hola mundo!")
+
+//EJERCICIO 2
+/* Escribir un programa que almacene la cadena ¡Hola Mundo! en una variable y luego muestre por
+pantalla el contenido de la variable. */
+
+let saludo = "¡Hola mundo!"
+alert(saludo)
+
+//EJERCICIO 3
+/* Escribir un programa que pregunte el nombre del usuario en la consola y después de que el usuario lo
+introduzca muestre por pantalla la cadena ¡Hola <nombre>!, donde <nombre> es el nombre que el
+usuario haya introducido. */
+
+let nombre = prompt("Ingrese su nombre")
+alert("¡Hola " + nombre + "!")
+
+//EJERCICIO 4
+//Escribir un programa que muestre por pantalla el resultado de la siguiente operación aritmética
+
+alert(Math.pow((3+2)/(2*5), 2))
+
+//EJERCICIO 5
+/* Escribir un programa que pregunte al usuario por el número de horas trabajadas y el coste por hora. 
+Después debe mostrar por pantalla la paga que le corresponde. */
+
+let horasTrabajadas = prompt("Ingrese la cantidad de horas trabajadas")
+let costePorHora = prompt("Ingrese el coste por hora")
+let totalPorDia = horasTrabajadas * costePorHora
+let totalPorMes = totalPorDia * 22
+
+alert("Coste total por día = " + totalPorDia + "\n" + 
+        "Coste total por mes = " + totalPorMes)
+
+//EJERCICIO 6
+/* Escribir un programa que pida al usuario su peso (en kg) y estatura (en metros), calcule el índice de 
+masa corporal y lo almacene en una variable, y muestre por pantalla la frase Tu índice de masa corporal
+es <imc> donde <imc> es el índice de masa corporal calculado redondeado con dos decimales. */
+
+let peso = prompt("Ingrese su peso en kgs.")
+let altura = prompt("Ingrese su estatura en metros")
+let imc = 
+
+//EJERCICIO 7
+
+
+//EJERCICIO 8
+
+
+//EJERCICIO 9
+
+
+//EJERCICIO 10
